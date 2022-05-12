983dd28d6d582f3c4602f9c16e18cf2c25e00f44
Pedro Baez <pedrobaezb@gmail.com>
2022-05-12 22:50:20 +0200

    Añadido el proyecto inicial generado por initializr

---
+++ src/main/java/com/sanitas/prueba4/pedro/calculadora/CalculadoraApplication.java
@@ -0,0 +1,13 @@
+package com.sanitas.prueba4.pedro.calculadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraApplication.class, args);
+	}
+
+}
